7957b2243aae9775634046842e78f7bdda7598e7
alexiscg17 <alexiscg_0417@hotmail.com>
2025-09-19 13:20:09 -0400

    Update routes

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -52,10 +52,49 @@ regd_users.post("/login", (req, res) => {
     }
 });
 
-// Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization.username;
+
+    let book = books[isbn];
+
+    if (!book) {
+        return res.status(404).json({ message: "Unable to find book!" });
+    }
+
+    if (!review) {
+        return res.status(400).json({ message: "Review text is required" });
+    }
+
+    book.reviews[username] = review;
+
+    return res.status(200).json({
+        message: "Review added or updated successfully",
+        reviews: book.reviews
+    });
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+
+    const book = books[isbn];
+    
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (!book.reviews[username]) {
+        return res.status(404).json({ message: "No review found for this user" });
+    }
+
+    delete book.reviews[username];
+
+    return res.status(200).json({
+        message: "Review deleted successfully",
+        reviews: book.reviews
+    });
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,31 +20,63 @@ public_users.post("/register", (req, res) => {
 });
 
 public_users.get('/', function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
+    res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn], null, 4));
+    let book = books[isbn];
+
+    if(book){
+        res.status(200).send(JSON.stringify(book, null, 4));
+    }
+    else{
+        res.status(404).json({ message: "Unable to find book." });
+    }
 });
 
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
     const allBooks = Object.values(books);
     const booksByAuthor = allBooks.filter(book => book.author === author);
-    res.send(booksByAuthor);
+    
+    if(booksByAuthor.length > 0){
+        res.status(200).send(booksByAuthor);
+    }
+    else{
+        res.status(404).json({ message: "Unable to find book by author." });
+    }
 });
 
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
     const allBooks = Object.values(books);
     const booksByTitle = allBooks.filter(book => book.title === title);
-    res.send(booksByTitle);
+    
+    if(booksByTitle.length > 0){
+        res.status(200).send(booksByTitle);
+    }
+    else{
+        res.status(404).json({ message: "Unable to find book by title." });
+    }
 });
 
 public_users.get('/review/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn].reviews, null, 4));
+    let book = books[isbn];
+    let reviews = books[isbn].reviews;
+
+    if(book){
+        if(reviews){
+            res.status(200).send(JSON.stringify(reviews, null, 4));
+        }
+        else{
+            res.status(404).json({ message: "Reviews not found." });
+        }
+    }
+    else{
+        
+    }
 });
 
 module.exports.general = public_users;
